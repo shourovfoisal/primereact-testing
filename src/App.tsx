7def266819d0d3c27d722cd6433ac1a057ea9f38
Shourov Foisal <shourovfoisal@gmail.com>
2023-07-08 15:36:23 +0600

    added basic routing with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 // import './App.css'
 
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./components/Home"
+import Menu from "./components/Menu"
+
 function App() {
 
-  return <></>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
